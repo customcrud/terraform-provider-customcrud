db605a4983070121521f1885071bd3bbb7d0b5c1
kumail.kermalli <kumail.kermalli@datatonic.com>
2025-07-02 18:00:38 +0100

    fix: cat for content, based on feedback

--- examples/file/hooks/read.sh
+++ examples/file/hooks/read.sh
@@ -5,5 +5,6 @@ set -e
 INPUT="$(cat)"
 1>&2 echo "[READ] INPUT_DUMP: $INPUT"
 ID="$(echo "$INPUT" | jq -r ".id")"
+content=$(cat "$ID") || exit 22
 
-jq -n --arg id "$ID" --arg content "$(cat "$ID")" '{id: $id, content: $content}'
+jq -n --arg id "$ID" --arg content "$content" '{id: $id, content: $content}'
